6682e2bf4d9077b3c1d0b3cff833a6c4d4f09a9f
陳冠宇 <chen630602@gmail.com>
2024-05-30 21:06:21 +0800

    Initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,345 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="banner">
+        <img src="https://firebasestorage.googleapis.com/v0/b/dogcat-d363b.appspot.com/o/%E8%B2%93%E7%8B%97%E5%8D%80%2F%E5%B0%81%E9%9D%A23.jpg?alt=media&token=bca7ce8d-54e7-483f-af24-b2c648ff5bb2">
+    </section>
+
+    <section class="container hotProducts">
+      <h2>熱門商品區(貓狗專區)</h2>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card">
+            <img src="https://firebasestorage.googleapis.com/v0/b/dogcat-d363b.appspot.com/o/%E8%B2%93%E8%B2%93%E5%8D%80%2F%E8%B2%93%E6%9C%A8%E5%A4%A9%E7%99%82.jpg?alt=media&token=4e227f7a-559b-425b-9830-496647325746">
+            <div class="card-body">
+              <div>
+                <h3 class="card-text">木天蓼</h3>
+                <p>讓貓寶貝放鬆的好夥伴</p>
+              </div>
+              <i class="bi bi-arrow-right"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://firebasestorage.googleapis.com/v0/b/dogcat-d363b.appspot.com/o/%E7%8B%97%E7%8B%97%E5%8D%80%2F%E7%B6%B2%E7%90%83.jpg?alt=media&token=2b366c64-7f40-41e6-8611-0c18db81e555">
+            <div class="card-body">
+              <div>
+                <h3 class="card-text">狗狗專用球球</h3>
+                <p>讓狗寶貝消耗體力的最佳選擇</p>
+              </div>
+              <i class="bi bi-arrow-right"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://firebasestorage.googleapis.com/v0/b/dogcat-d363b.appspot.com/o/%E7%8B%97%E7%8B%97%E5%8D%80%2F%E5%B8%B3%E6%A3%9A%E7%8B%97%E7%8B%97.jpg?alt=media&token=f44120be-9533-4229-af12-e9ef22241c5c">
+            <div class="card-body">
+              <div>
+                <h3 class="card-text">寵物專用帳篷</h3>
+                <p>讓狗寶貝睡眠睡得安穩</p>
+              </div>
+              <i class="bi bi-arrow-right"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container marketing onSale">
+        <div class="row featurette">
+            <div class="col-md-6 order-md-2 sale">
+                <h1 class="pt-5">20%OFF</h1>
+                <h1>正在促銷中</h1>
+                <h4>最低消費费 NT100 享 20% 折扣</h4>
+                <h5>2024/5/1 ~ 2024/12/31</h5>
+                <a class="btn " href="#" role="button">SHOP NOW</a>
+            </div>
+            
+          <div class="col-md-6 order-md-1">
+            <img class="img-fluid" src="https://firebasestorage.googleapis.com/v0/b/dogcat-d363b.appspot.com/o/%E8%B2%93%E7%8B%97%E5%8D%80%2F%E5%B0%81%E9%9D%A21.jpg?alt=media&token=f4e03cc5-51b8-4e81-8aab-3418fbaae763">
+        </div>
+      </div>
+    </section>
+
+    <section class="container newProduct">
+      <h2>新品上市(貓狗專區)</h2>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-3">
+        <div class="col">
+          <div class="card">
+            <img src="https://firebasestorage.googleapis.com/v0/b/dogcat-d363b.appspot.com/o/%E8%B2%93%E8%B2%93%E5%8D%80%2F%E8%B2%93%E7%AA%A9.jpg?alt=media&token=5f513287-6727-49bc-bac0-0547e5926ad5">
+            <div class="card-body">
+              <div>
+                <h3 class="card-text">貓窩</h3>
+                <p>讓貓寶貝享受休息的時光</p>
+              </div>
+              <i class="bi bi-arrow-right"></i>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="https://firebasestorage.googleapis.com/v0/b/dogcat-d363b.appspot.com/o/%E7%8B%97%E7%8B%97%E5%8D%80%2Fchewy%E9%A3%9B%E7%9B%A4.jpg?alt=media&token=84de32ed-0ab3-4690-b005-2721a042cfa0">
+            <div class="card-body">
+              <div>
+                <h3 class="card-text">狗狗飛盤</h3>
+                <p>讓主人和狗寶貝傳接飛盤</p>
+              </div>
+              <i class="bi bi-arrow-right"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container positiveFeedback">
+      <h2>新品上市(貓狗專區)</h2>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <div class="d-flex align-items-center ">
+                <img src="https://firebasestorage.googleapis.com/v0/b/dogcat-d363b.appspot.com/o/%E5%A5%BD%E8%A9%95%E5%9B%9E%E9%A5%8B%2Fjohn.png?alt=media&token=0a5f4c8f-b427-49b5-ad68-3eb84982a694">
+                <div>
+                  <h4>John</h4>
+                  <h5>貓咪愛好者</h5>
+                </div>
+              </div>
+              <p class="pt-4">這款貓窩真是太棒了！我家的貓咪一開始有點猶豫，但一旦躺進去就不想出來了。貓窩的材質非常柔軟，感覺很舒適，而且內襯的質地很好，對貓咪的皮膚非常友好。看起來也很耐用，已經使用了好幾個月依然如新。強烈推薦給所有養貓的朋友！</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <div class="d-flex align-items-center ">
+                <img src="https://firebasestorage.googleapis.com/v0/b/dogcat-d363b.appspot.com/o/%E5%A5%BD%E8%A9%95%E5%9B%9E%E9%A5%8B%2FKitty.png?alt=media&token=d68beb9c-7fff-4007-be5f-c63f97bcb3e0">
+                <div>
+                  <h4>kitty</h4>
+                  <h5>狗狗帳篷</h5>
+                </div>
+              </div>
+              <p class="pt-4">這款狗狗帳篷的質量非常出色！材料堅固耐用，能夠抵擋狗狗的抓撓和咬咬。帳篷的支架也非常穩固，即使狗狗在裡面玩耍也不會倒塌。整體設計非常耐用，我相信可以使用很長一段時間。非常值得購買！</p>
+            </div>
+          </div>
+        </div> <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <div class="d-flex align-items-center ">
+                <img src="https://firebasestorage.googleapis.com/v0/b/dogcat-d363b.appspot.com/o/%E5%A5%BD%E8%A9%95%E5%9B%9E%E9%A5%8B%2Fcindy.png?alt=media&token=be8bb1eb-f2c1-45fb-b3cc-0d50958fe76d">
+                <div>
+                  <h4>Cindy</h4>
+                  <h5>狗狗飛盤</h5>
+                </div>
+              </div>
+              <p class="pt-4">這款貓窩真是太棒了！我家的貓咪一開始有點猶豫，但一旦躺進去就不想出來了。貓窩的材質非常柔軟，感覺很舒適，而且內襯的質地很好，對貓咪的皮膚非常友好。看起來也很耐用，已經使用了好幾個月依然如新。強烈推薦給所有養貓的朋友！</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+@media (max-width: 768px) {
+  h2{
+    text-align: center;
   }
 }
-</script>
+
+
+.hotProducts .card-body,
+.newProduct .card-body,
+.home{
+  background-color: #fffff6;
+}
+
+
+
+.banner img{
+  width: 100%;
+  max-height: 600px;
+  object-fit:cover;
+}
+
+.home h2{
+  color:#B95D3B;
+  font-size:32px;
+  font-weight: bold ;
+  margin-bottom:75px;
+}
+
+.hotProducts{
+  margin:50px 0;
+}
+
+.hotProducts .card-body{
+  display:flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.hotProducts .card-body i{
+  font-size:20px;
+}
+
+.newProduct .card,
+.hotProducts .card{
+  border:none !important;
+}
+
+.hotProducts img{
+  width: auto;
+  height: 500px;
+  border-radius: 20px;
+  object-fit: cover;
+}
+
+.hotProducts .card-body{
+  padding-top:28px;
+}
+
+.hotProducts .card-body h3{
+  font-size:24px;
+  color:#924444;
+}
+
+.hotProducts .card-body p{
+  margin:0px;
+  font-size:20px;
+  color:#7A6969;
+}
+
+
+.onSale{
+  margin-top:150px;
+  padding-bottom:100px;
+}
+
+.onSale .order-md-1 img{
+  width:600px;
+  height:500px;
+  object-fit: cover;
+  border-radius: 5px;;
+}
+
+@media (max-width: 768px) {
+  .onSale .row {
+    flex-direction: column-reverse;
+  }
+}
+
+
+.onSale .row .sale{
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+@media (max-width: 768px) {
+  .onSale .row .sale{
+    align-items: center;
+  }
+}
+
+.onSale .row h1{
+  font-size:60px;
+  font-weight: bold;
+  color:#B95D3B
+}
+
+.onSale .row h4{
+  font-size:32px;
+  color:#B95D3B;
+  padding-top:25px;
+}
+
+.onSale .row h5{
+  font-size:24px;
+  color:#B95D3B;
+  opacity: 0.4;
+  padding-top:15px
+}
+
+.onSale .row a{
+  margin-top:50px;
+  background-color: rgba(185, 93, 59, 0.8);
+  color:white;
+  font-size:20px;
+  width: 180px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.newProduct{
+  padding-bottom:50px;
+  margin-bottom:50px;
+}
+
+.newProduct .card-body{
+  display:flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.newProduct img{
+  width: auto;
+  height: 450px;
+  border-radius: 10px;
+  object-fit: cover;
+}
+
+.newProduct .card-body{
+  padding-top:28px;
+}
+
+.newProduct .card-body h3{
+  font-size:24px;
+  color:#924444;
+}
+
+.newProduct .card-body p{
+  margin:0px;
+  font-size:20px;
+  color:#7A6969;
+}
+
+.positiveFeedback{
+  padding-bottom:100px;
+}
+
+.positiveFeedback .card{
+  height: auto;
+  box-shadow: 4px 4px 5px rgba(0, 0, 0, 0.2);
+}
+
+.positiveFeedback .card-body{
+  padding:30px;
+}
+.positiveFeedback .card-body img{
+  margin-right:10px;
+}
+
+.positiveFeedback .card-body h4{
+  font-size:16px;
+  color:black ;
+  margin-bottom:5px;
+}
+
+.positiveFeedback .card-body h5{
+  font-size:16px;
+  color:#828282 ;
+  margin-bottom:0px;
+}
+
+.positiveFeedback .card-body p{
+  font-size:16px;
+  color:#555151;
+}
+</style>
